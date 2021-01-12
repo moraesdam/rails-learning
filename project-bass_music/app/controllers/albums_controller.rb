e66c9938aec0262252174b8be51b6ebe9e50dcf0
codecademy <ccuser@codecademy.com>
2021-01-12 00:15:46 +0000

    add project Bass Music

---
+++ project-bass_music/app/controllers/albums_controller.rb
@@ -0,0 +1,12 @@
+class AlbumsController < ApplicationController
+
+  def index
+    @albums = Album.all
+  end
+
+  def show
+    @album = Album.find(params[:id])
+    @tracks = @album.track.all
+  end
+
+end
